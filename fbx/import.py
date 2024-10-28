b432817944fc03042fb5d1df7e7e4f4ba1de7496
tourss <67784154+tourss@users.noreply.github.com>
2024-10-28 13:24:16 +0900

    Update import.py

--- fbx/import.py
+++ fbx/import.py
@@ -5,11 +5,12 @@ import unreal
 # path
 destination_path = "/Game/fbx_python/pear"  # 언리얼 자산 디렉토리 위치
 source_path = r"C:\Users\admin\Desktop\fbx\pear\pear.fbx"  # 로컬 자산 디렉토리 위치
+assets_to_import = list(Path(source_path).glob("*.fbx"))
 
 # import data 
-fbx_import_data = unreal.FbxSkeletalMeshImportData()
-# fbx_import_data.combine_meshes = True
-# fbx_import_data.remove_degenerates = True
+static_mesh_import_data = unreal.FbxStaticMeshImportData()
+static_mesh_import_data.combine_meshes = True
+static_mesh_import_data.remove_degenerates = True
 
 # import options setting
 options = unreal.FbxImportUI()
@@ -25,12 +26,12 @@ options.skeletal_mesh_import_data = fbx_import_data
 tasks: List[unreal.AssetImportTask] = []
 
 # generate import tasks
-if Path(source_path).exists():
+for input_file_path in assets_to_import:
     task = unreal.AssetImportTask()
     task.automated = True
     task.destination_path = destination_path
-    task.destination_name = Path(source_path).stem  # 파일 이름을 자산 이름으로 사용
-    task.filename = str(source_path)
+    task.destination_name = Path(input_file_path).stem  # 파일 이름을 자산 이름으로 사용 #안되면 input_file_path.stem
+    task.filename = str(input_file_path)
     task.replace_existing = True
     task.save = True
     task.options = options
@@ -38,7 +39,7 @@ if Path(source_path).exists():
     tasks.append(task)
 
 # execute import tasks
-unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks(tasks)
+unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks(unreal.Array(unreal.Asset.cast(unreal.AssetImportTask, tasks)
 
 # log print
 for task in tasks:
